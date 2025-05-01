0e9aa3a3d88c6d38276ef38b7eea883c38002061
thisago <thisago@disroot.org>
2025-05-01 15:02:24 -0300

    feat: initial version

--- publish.el
+++ publish.el
@@ -1,27 +1,3 @@
-;;; publish.el --- thisago's blog -*- lexical-binding: t; -*-
-;;
-;; Copyright (C) 2025
-;;
-;; Author:  <thisago@disroot.org>
-;; Maintainer:  <thisago@disroot.org>
-;; Created: April 21, 2025
-;; Modified: April 21, 2025
-;; Version: 0.0.1
-;; Keywords: blog
-;; Homepage: https://thisago.com
-;; Package-Requires: ((emacs "24.3"))
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; Commentary:
-;;
-;; Stolen/Inspired from:
-;; - https://github.com/SystemCrafters/org-website-example/blob/1ee251e97f5b4d6c614936030203cd7368d4adc8/build-site.el
-;; - https://orgmode.org/worg/org-site-colophon.html
-;
-;;
-;;; Code:
-
 (require 'package)
 (setq package-user-dir (expand-file-name "./.packages"))
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -32,61 +8,67 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Install dependencies
-(unless (package-installed-p 'org-static-blog)
-  (package-install 'org-static-blog))
+;; Install htmlize for syntax highlighting
+(unless (package-installed-p 'htmlize)
+  (package-install 'htmlize))
 
-(setq org-static-blog-publish-title "thisago's blog")
-(setq org-static-blog-publish-url "https://thisago.com")
-(setq org-static-blog-publish-directory "~/Documents/repos/blog/static/")
-(setq org-static-blog-posts-directory "~/Documents/repos/blog/posts/")
-(setq org-static-blog-drafts-directory "~/Documents/repos/blog/drafts/")
-(setq org-static-blog-enable-tags t)
-(setq org-export-with-toc nil)
-(setq org-export-with-section-numbers nil)
+(require 'org)
 
-;; This header is inserted into the <head> section of every page:
-;;   (you will need to create the style sheet at
-;;    ~/projects/blog/static/style.css
-;;    and the favicon at
-;;    ~/projects/blog/static/favicon.ico)
+;; Enable syntax highlighting
+(setq org-src-fontify-natively t)
+
+;; Load the publishing system
+(require 'ox-publish)
+
+;; Build SASS
+(defun build-sass (main-file output-file)
+  "Build SASS MAIN-FILE using the command line into OUTPUT-FILE."
+  (unless (executable-find "sass")
+    (error "SASS is not installed or not in your PATH"))
+  (make-directory (file-name-directory output-file) t)
+  (shell-command (format "sass --no-source-map -s compressed %s %s" main-file output-file)))
+(build-sass "theme/index.sass" "public/theme/style.css")
+
+(defun read-file (filename)
+  "Read the contents of the FILENAME and return it as a string."
+  (with-temp-buffer
+    (insert-file-contents filename)
+    (buffer-string)))
+
+;; Customize the HTML output
+(setq org-html-head-include-default-style nil
+      org-html-head (read-file "templates/head.html")
+      org-html-preamble (read-file "templates/preamble.html")
+      org-html-postamble (read-file "templates/postamble.html"))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((shell . t)
+   (dot . t)
+   (org . t)))
 
 (setq
- head-author "thisago"
- head-description "thisago's blog"
- head-viewport "width=device-width, initial-scale=1.0"
- head-stylesheet "static/style.css"
- head-icon "static/favicon.ico")
+ org-html-doctype "html5"
+ org-confirm-babel-evaluate nil
+ make-backup-files nil
+ org-src-fontify-natively t
+ org-publish-project-alist (list
+                            (list "org-site:main"
+                                  :recursive t
+                                  :base-directory "./posts"
+                                  :publishing-function 'org-html-publish-to-html
+                                  :publishing-directory "./public"
+                                  :exclude "drafts"
+                                  :with-author nil
+                                  :with-toc t
+                                  :time-stamp-file t
+                                  :section-numbers nil
+                                  :auto-sitemap t
+                                  :sitemap-filename "index.org"
+                                  :sitemap-title "Posts"
+                                  :with-timestamps t
+                                  :with-footnotes t)))
 
-(setq org-static-blog-page-header
-      (concat
-       "<meta name=\"author\" content=\"" head-author "\">"
-       "<meta name=\"referrer\" content=\"no-referrer\">"
-       "<meta name=\"viewport\" content=\"" head-viewport "\">"
-       "<link href=\"" head-stylesheet "\" rel=\"stylesheet\" type=\"text/css\" />"
-       "<link rel=\"icon\" href=\"" head-icon "\"/>"))
+(org-publish-all t)
 
-;; This preamble is inserted at the beginning of the <body> of every page:
-;;   This particular HTML creates a <div> with a simple linked headline
-(setq org-static-blog-page-preamble
-      (concat
-       "<div class=\"header\">
-          <a href=\"" org-static-blog-publish-url "\">My Static Org Blog</a>
-        </div>"))
-
-;; This postamble is inserted at the end of the <body> of every page:
-;;   This particular HTML creates a <div> with a link to the archive page
-;;   and a licensing stub.
-(setq org-static-blog-page-postamble
-      "<div id=\"archive\">
-       <a href=\"https://staticblog.org/archive.html\">Other posts</a>
-     </div>
-     <center><a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"https://i.creativecommons.org/l/by-sa/3.0/88x31.png\" /></a><br /><span xmlns:dct=\"https://purl.org/dc/terms/\" href=\"https://purl.org/dc/dcmitype/Text\" property=\"dct:title\" rel=\"dct:type\">bastibe.de</span> by <a xmlns:cc=\"https://creativecommons.org/ns#\" href=\"https://bastibe.de\" property=\"cc:attributionName\" rel=\"cc:attributionURL\">Bastian Bechtold</a> is licensed under a <a rel=\"license\" href=\"https://creativecommons.org/licenses/by-sa/3.0/\">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.</center>")
-
-;; This HTML code is inserted into the index page between the preamble and
-;;   the blog posts
-(setq org-static-blog-index-front-matter
-      "<h1> Welcome to my blog </h1>\n")
-
-(provide 'publish)
-;;; publish.el ends here
+(message "Build complete!")
